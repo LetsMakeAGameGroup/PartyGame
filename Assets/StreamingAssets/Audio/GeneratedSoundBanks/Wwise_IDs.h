099a0cd4e8cb421db134c8b42a3ca4c851f52460
Jason <77205664+Saltdaddy2806@users.noreply.github.com>
2023-06-14 13:58:37 +0100

    Added PlayerLocomotion & new Soundbank
    Added PlayerLocomotion & new Soundbank

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,16 +11,74 @@
 
 namespace AK
 {
-    namespace BANKS
+    namespace STATES
     {
-        static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID MAIN_SOUNDBANK = 2228651116U;
-    } // namespace BANKS
+        namespace GAMESTATUS
+        {
+            static const AkUniqueID GROUP = 1045871717U;
+
+            namespace STATE
+            {
+                static const AkUniqueID INGAME = 984691642U;
+                static const AkUniqueID INMENU = 3374585465U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace GAMESTATUS
+
+        namespace PLAYERSTATE
+        {
+            static const AkUniqueID GROUP = 3285234865U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ALIVE = 655265632U;
+                static const AkUniqueID DEAD = 2044049779U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace PLAYERSTATE
+
+        namespace ROOMSTATE
+        {
+            static const AkUniqueID GROUP = 185713839U;
+
+            namespace STATE
+            {
+                static const AkUniqueID INDOOR = 340398852U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID OUTDOOR = 144697359U;
+            } // namespace STATE
+        } // namespace ROOMSTATE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace GROUNDMATERIALSWITCH
+        {
+            static const AkUniqueID GROUP = 1044534455U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID STONE = 1216965916U;
+            } // namespace SWITCH
+        } // namespace GROUNDMATERIALSWITCH
+
+    } // namespace SWITCHES
 
     namespace BUSSES
     {
+        static const AkUniqueID AMBIENTBEDS = 1182634443U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
     } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID LARGEREVERB = 3684725634U;
+        static const AkUniqueID REVERB = 348963605U;
+        static const AkUniqueID SMALLREVERB = 2134960294U;
+    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
