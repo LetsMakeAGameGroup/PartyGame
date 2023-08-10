c5d6e68c7a13fc6b27eba7718a1a92fafcf73d82
Dannan Vazquez <dannanvazquez@gmail.com>
2023-08-10 04:49:36 -0700

    Revise Territories In Wisp Whisk
    Territories now take inspiration from Flower Child's Flower Spirit. Territories now consume the wisp and itself for bonus points, then respawns a new wisp and territory.

--- Assets/Code/Scripts/Managers/WispWhiskManager.cs
+++ Assets/Code/Scripts/Managers/WispWhiskManager.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 public class WispWhiskManager : NetworkBehaviour {
     [Header("References")]
     [SerializeField] private MinigameHandler minigameHandler;
-    [SerializeField] private GameObject[] territories;
+    [SerializeField] private GameObject territoryPrefab;
     [SerializeField] private GameObject wispPrefab;
     [SerializeField] private Canvas scoreDisplayCanvas = null;
     [SerializeField] private TextMeshProUGUI scoreDisplayText = null;
@@ -16,16 +16,16 @@ public class WispWhiskManager : NetworkBehaviour {
     [Header("Settings")]
     [Tooltip("How many seconds between wisps assign points to their holding player.")]
     [SerializeField] private float timeBetweenGivenPoints = 0.25f;
-    [Tooltip("Minimum or bottom left 2D vector(X and Z-Axis) of where wisps can spawn in an area.")]
-    [SerializeField] private Vector2 minWispSpawnLocation = Vector2.zero;
-    [Tooltip("Maximum or top right 2D vector(X and Z-Axis) of where wisps can spawn in an area.")]
-    [SerializeField] private Vector2 maxWispSpawnLocation = Vector2.zero;
+    [Tooltip("Minimum or bottom left 2D vector(X and Z-Axis) of where wisps/territories can spawn in an area.")]
+    [SerializeField] private Vector2 minSpawnLocation = Vector2.zero;
+    [Tooltip("Maximum or top right 2D vector(X and Z-Axis) of where wisps/territories can spawn in an area.")]
+    [SerializeField] private Vector2 maxSpawnLocation = Vector2.zero;
     [Tooltip("How far from the ground the wisp will spawn.")]
     [SerializeField] private float distanceFromGroundWispSpawn = 0.5f;
     [Tooltip("The minimum distance between two wisps when spawning.")]
     [SerializeField] private float distanceBetweenWisps = 1f;
 
-    private Dictionary<GameObject, int> playerPoints = new();
+    [HideInInspector] public Dictionary<GameObject, int> playerPoints = new();
 
     // Called by server when the minigame starts.
     public void SpawnWisps() {
@@ -33,22 +33,23 @@ public class WispWhiskManager : NetworkBehaviour {
             playerPoints.Add(player.identity.gameObject, 0);
         }
 
-        //Spawnwisps here
+        //Spawn wisps and territories here
         int wispCount = 1;
         if (CustomNetworkManager.Instance.ClientDatas.Count > 4) wispCount = 2;
         if (CustomNetworkManager.Instance.ClientDatas.Count > 6) wispCount = 3;
 
         for (int i = 0; i < wispCount; i++) {
             StartCoroutine(SpawnWisp());
+            StartCoroutine(SpawnTerritory());
         }
         RpcEnableScoreDisplay();
 
         StartCoroutine(AddPointsEveryInterval());
     }
 
-    private IEnumerator SpawnWisp() {
+    public IEnumerator SpawnWisp() {
         while (true) {
-            Vector2 overheadLocation = new Vector2(Random.Range(minWispSpawnLocation.x, maxWispSpawnLocation.x + 1), Random.Range(minWispSpawnLocation.y, maxWispSpawnLocation.y + 1));
+            Vector2 overheadLocation = new Vector2(Random.Range(minSpawnLocation.x, maxSpawnLocation.x + 1), Random.Range(minSpawnLocation.y, maxSpawnLocation.y + 1));
 
             int excludePlayerLayerMask = ~LayerMask.GetMask("Player");
             if (Physics.Raycast(new Vector3(overheadLocation.x, 10f, overheadLocation.y), Vector3.down, out RaycastHit hit, 15f, excludePlayerLayerMask)) {
@@ -74,9 +75,24 @@ public class WispWhiskManager : NetworkBehaviour {
             }
             yield return null;
         }
+    }
 
-        yield return null;
-        StartCoroutine(SpawnWisp());
+    public IEnumerator SpawnTerritory() {
+        while (true) {
+            Vector2 overheadLocation = new Vector2(Random.Range(minSpawnLocation.x, maxSpawnLocation.x + 1), Random.Range(minSpawnLocation.y, maxSpawnLocation.y + 1));
+
+            int excludePlayerLayerMask = ~LayerMask.GetMask("Player");
+            if (Physics.Raycast(new Vector3(overheadLocation.x, 10f, overheadLocation.y), Vector3.down, out RaycastHit hit, 15f, excludePlayerLayerMask)) {
+                // Don't need to check for nearby territories since they immediately start moving anyways.
+                GameObject territory = Instantiate(territoryPrefab, hit.point, Quaternion.identity);
+                territory.GetComponent<Territory>().wispWhiskManager = this;
+                NetworkServer.Spawn(territory);
+                StartCoroutine(territory.GetComponent<RandomlyMovingAgent>().MoveTowardsTrans());
+                break;
+            }
+
+            yield return null;
+        }
     }
 
     /// <summary>Enable score display on all clients.</summary>
@@ -89,15 +105,7 @@ public class WispWhiskManager : NetworkBehaviour {
         foreach (var player in CustomNetworkManager.Instance.ClientDatas.Keys) {
             if (player.identity.GetComponent<WispEffect>() && player.identity.GetComponent<WispEffect>().holdingWisp) {
                 int pointsToAdd = player.identity.GetComponent<WispEffect>().holdingWisp.GetComponent<CollectableWispEffect>().pointsToAdd;
-                foreach (var territory in territories) {
-                    if (territory.GetComponent<ContainPlayersInsideCollider>().playersInside.Contains(player.identity.gameObject)) {
-                        pointsToAdd += (int)(pointsToAdd * territory.GetComponent<ContainPlayersInsideCollider>().pointsMultiplier);
-                        break;
-                    }
-                }
-
                 playerPoints[player.identity.gameObject] += pointsToAdd;
-
                 TargetSetScoreDisplay(player.identity.GetComponent<NetworkIdentity>().connectionToClient, playerPoints[player.identity.gameObject]);
             }
         }
@@ -107,7 +115,7 @@ public class WispWhiskManager : NetworkBehaviour {
     }
 
     [TargetRpc]
-    private void TargetSetScoreDisplay(NetworkConnectionToClient target, int score) {
+    public void TargetSetScoreDisplay(NetworkConnectionToClient target, int score) {
         scoreDisplayText.text = score.ToString();
     }
 
