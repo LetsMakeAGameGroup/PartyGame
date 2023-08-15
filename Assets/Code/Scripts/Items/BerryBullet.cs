c8ff015b01d79deddaf2ed811d79df855ed03fc9
Dannan Vazquez <dannanvazquez@gmail.com>
2023-08-15 08:31:40 -0700

    Berry Blaster + Berry Projectile Models
    Added models for the Berry Blaster and Berry projectile. No animations for equipping items so this will have to do.

--- Assets/Code/Scripts/Items/BerryBullet.cs
+++ Assets/Code/Scripts/Items/BerryBullet.cs
@@ -2,6 +2,9 @@ using Mirror;
 using UnityEngine;
 
 public class BerryBullet : Bullet {
+    [Header("Berry Bullet References")]
+    [SerializeField] private Renderer berryRenderer;
+
     [Header("Berry Bullet Settings")]
     [Tooltip("The amount of force this will knock the hit entity backwards.")]
     [SerializeField] private float knockbackForce = 25f;
@@ -13,7 +16,7 @@ public class BerryBullet : Bullet {
     [SyncVar(hook = nameof(SetColor)), HideInInspector] public string bulletColor = "";
 
     public void SetColor(string oldColor, string newColor) {
-        GetComponent<Renderer>().material.color = PlayerColorOptions.options[newColor];
+        berryRenderer.material.color = PlayerColorOptions.options[newColor];
     }
 
     public override void OnHit(GameObject hitObject) {
