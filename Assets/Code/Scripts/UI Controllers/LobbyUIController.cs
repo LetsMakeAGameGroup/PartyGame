4e920c515c186426098f5f1731d4e858bfae714c
Dannan Vazquez <dannanvazquez@gmail.com>
2023-08-15 16:39:33 -0700

    Basic Lobby Menu
    Simple menu while in the lobby to copy the lobby code and start the game.

--- Assets/Code/Scripts/UI Controllers/LobbyUIController.cs
+++ Assets/Code/Scripts/UI Controllers/LobbyUIController.cs
@@ -1,9 +1,40 @@
+using Mirror;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class LobbyUIController : MonoBehaviour {
+public class LobbyUIController : NetworkBehaviour {
     [Header("References")]
     public TextMeshProUGUI codeText;
+    [SerializeField] private GameObject escapeMenu;
+    [SerializeField] private Button startButton;
 
-    private void Start() => FindObjectOfType<LobbyUIController>().codeText.text = CustomNetworkManager.Instance.relayJoinCode.ToUpper();
+    private void Start() {
+        FindObjectOfType<LobbyUIController>().codeText.text = CustomNetworkManager.Instance.relayJoinCode.ToUpper();
+
+        if (isServer) {
+            startButton.interactable = true;
+        }
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            escapeMenu.SetActive(NetworkClient.localPlayer.GetComponent<PlayerMovementComponent>().CanMove);
+        }
+    }
+
+    public void ToggleMenu() {
+        escapeMenu.SetActive(NetworkClient.localPlayer.GetComponent<PlayerMovementComponent>().CanMove);
+        NetworkClient.localPlayer.GetComponent<PlayerController>().TogglePause();
+    }
+
+    public void CopyLobbyCode() {
+        GUIUtility.systemCopyBuffer = CustomNetworkManager.Instance.relayJoinCode.ToUpper();
+    }
+
+    public void StartGame() {
+        if (GameManager.Instance.round == 0) {
+            GameManager.Instance.StartNextRound();
+        }
+    }
 }
