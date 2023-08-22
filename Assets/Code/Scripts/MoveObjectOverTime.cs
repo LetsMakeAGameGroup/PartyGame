ceeb4346ee6951f3be8b695fa170b0e49de3ab6d
Dannan Vazquez <dannanvazquez@gmail.com>
2023-08-21 22:31:21 -0700

    Add Pauses Between MoveObjectOverTime Positions
    Added pauseDuration variable to have an object stop for its seconds once reaching each position in the list.

--- Assets/Code/Scripts/MoveObjectOverTime.cs
+++ Assets/Code/Scripts/MoveObjectOverTime.cs
@@ -1,4 +1,5 @@
 using Mirror;
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(NetworkTransform))]
@@ -10,6 +11,8 @@ public class MoveObjectOverTime : NetworkBehaviour {
     [Header("Settings")]
     [Tooltip("How fast the object will move.")]
     [SerializeField] private float moveSpeed = 1f;
+    [Tooltip("The amount of seconds the object will stop moving for once reaching each position.")]
+    [SerializeField] private float pauseDuration = 0f;
     [Tooltip("If this object should immediately start moving. If not, it will only start moving once the current minigame starts.")]
     public bool canMove = false;
 
@@ -22,16 +25,27 @@ public class MoveObjectOverTime : NetworkBehaviour {
         }
     }
 
-    private void Update() {
-        if (!isServer || !canMove) return;
+    private void Start() {
+        if (!isServer) return;
 
-        if (Vector3.Distance(gameObject.transform.position, pathLocations[pathIndex]) > 0.01f) {
-            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, pathLocations[pathIndex], Time.deltaTime * moveSpeed);
-        } else {
-            gameObject.transform.position = pathLocations[pathIndex];
+        StartCoroutine(UpdateObject());
+    }
 
-            pathIndex++;
-            pathIndex %= pathLocations.Length;
+    private IEnumerator UpdateObject() {
+        while (!canMove) yield return null;  // Wait till the object can move.
+
+        while (true) {
+            if (Vector3.Distance(gameObject.transform.position, pathLocations[pathIndex]) > 0.01f) {
+                gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, pathLocations[pathIndex], Time.deltaTime * moveSpeed);
+            } else {
+                yield return new WaitForSeconds(pauseDuration);
+
+                gameObject.transform.position = pathLocations[pathIndex];
+
+                pathIndex++;
+                pathIndex %= pathLocations.Length;
+            }
+            yield return null;
         }
     }
 }
