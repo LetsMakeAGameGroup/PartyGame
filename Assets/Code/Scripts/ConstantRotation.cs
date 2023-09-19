4fddf0524eb1f0aa058a37122c2b46568bb5701f
Dannan Vazquez <dannanvazquez@gmail.com>
2023-09-19 04:34:01 -0700

    Client-sided Constant Rotation
    When the server calls Constant Rotation to start rotating, adjust the rotation according to the delay the call was received. At this point, the client should completely handle the rotation for the remainder and still be synced.

--- Assets/Code/Scripts/ConstantRotation.cs
+++ Assets/Code/Scripts/ConstantRotation.cs
@@ -1,7 +1,6 @@
 using Mirror;
 using UnityEngine;
 
-[RequireComponent(typeof(NetworkTransformReliable))]
 public class ConstantRotation : NetworkBehaviour {
     [Header("Settings")]
     [Tooltip("What axis the object should rotate around. An increased number in a single axis means it will move faster around that axis.")]
@@ -11,14 +10,24 @@ public class ConstantRotation : NetworkBehaviour {
 
     private void Awake() {
         if (rotateDirection == Vector3.zero) {
-            Debug.LogError("rotateDirection on ConstantRotation is empty. Destroying this ConstantRotation as it's not being used.", gameObject);
+            Debug.LogWarning("rotateDirection on ConstantRotation is empty. Destroying this ConstantRotation as it's not being used.", gameObject);
             Destroy(this);
         }
     }
 
-    private void Update() {
-        if (!isServer || !canMove) return;
+    private void FixedUpdate() {
+        if (!canMove) return;
 
-        transform.Rotate(Time.deltaTime * rotateDirection);
+        transform.Rotate(Time.fixedDeltaTime * rotateDirection);
+    }
+
+    [ClientRpc]
+    public void RpcStartRotation(Quaternion currentRotation) {
+        float delay = (float)(NetworkClient.connection.remoteTimeStamp / 1000);
+
+        transform.rotation = currentRotation;
+        transform.Rotate(delay * rotateDirection);
+
+        canMove = true;
     }
 }
